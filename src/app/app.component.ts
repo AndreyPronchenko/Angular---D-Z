e5675eb9871782e087190c757f3933f7a97ed709
Андрей <139353883+AndreyPronchenko@users.noreply.github.com>
2024-04-02 13:52:31 +0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  appTitle = 'Patience is a key element of success';
+  appTitle1 = 'If you think your teacher is tough, wait till you get a boss';
+  appTitle2 = 'Life is not fair — get used to it!';
+  appTitle3 = 'Success is a lousy teacher. It seduces smart people into thinking they can’t lose';
+  appTitle4 = 'Be nice to nerds. Chances are you’ll end up working for one';
+  appTitle5 = 'Сооснователь компании Microsoft';
+
+  appLife = 'Уи́льям Ге́нри Гейтс III (англ. William Henry Gates III; 28 октября 1955, Сиэтл, Вашингтон), более известный как Билл Гейтс (англ. Bill Gates) — американский предприниматель и общественный деятель, филантроп, один из создателей (совместно с Полом Алленом) и бывший крупнейший акционер компании Microsoft. До июня 2008 года являлся руководителем компании; после ухода с поста остался в должности её неисполнительного председателя совета директоров. Также является сопредседателем благотворительного Фонда Билла и Мелинды Гейтс, членом совета директоров Berkshire Hathaway, генеральным директором Cascade Investment[en].В период с 1996 по 2007 год, с 2009 по 2016 год — самый богатый человек планеты по версии журнала Forbes. Билл Гейтс является одним из рекордсменов по размеру средств, переданных на благотворительность: в период с 1994 по 2010 год он вложил в Фонд Билла и Мелинды Гейтс более 28 млрд долларов. В 2010 году Гейтс подписал «Клятву дарения» и выступил с предложением ко всем миллиардерам о передаче половины своих состояний на благотворительную деятельность'
+
+  appName = 'Bill Gates';
+  appImage = 'https://www.mann-ivanov-ferber.ru/assets/media/authors-new/billG.jpg';
+
+  appText = 'Cсылки на ресурсы с информацией о Гейтсе:';
+  appText1 = 'Цитаты:';
+
+  appUrl = 'https://en.wikipedia.org/wiki/Bill_Gates';
+  appUrl1 = 'https://www.gatesfoundation.org/';
+  appUrl2 = 'https://www.gatesnotes.com/';
+  appUrl3 = 'https://twitter.com/billgates';
+}
